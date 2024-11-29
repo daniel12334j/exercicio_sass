8f74fbef75171c1e55bc8d74dcb7e3a29610d918
daniel12334j <daniel12334l@outlook.com>
2024-11-29 17:49:06 -0300

    projeto completo

---
+++ base_exercicio_css_in_js-main/src/components/Cabecalho/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Cabecalho.module.css'
+import { Cabecalhos } from './Cabecalho'
+
+const Cabecalho = () => (
+  <Cabecalhos>
+    <h1>EBAC Jobs</h1>
+  </Cabecalhos>
+)
+
+export default Cabecalho
---
+++ base_exercicio_css_in_js-main/src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { FormHero, FormDiv, HeroTitle } from './HeroTs'
+
+const Hero = () => (
+  <FormHero>
+    <FormDiv className="container">
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </FormDiv>
+  </FormHero>
+)
+
+export default Hero
